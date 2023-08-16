372371d6f0814917faf361b7c2c74fa82620ed06
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-08-16 14:21:47 -0600

    Searching TODOs

--- src/App.js
+++ src/App.js
@@ -22,8 +22,16 @@ function App() {
     todo => !!todo.completed
     ).length;
   const totalTodos = todos.length;
+  const searchedTodos = todos.filter(
+    (todo) => { 
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      
+      return todoText.includes(searchText)
+  });
 
   console.log('Users are looking for: ' + searchValue);
+  console.log(searchedTodos);
 
   return (
     <>
@@ -37,7 +45,7 @@ function App() {
       />
         
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text}
           text={todo.text}
