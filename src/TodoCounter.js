2971c070a343d7ec058c094f36635265424150e0
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-09-06 12:57:15 -0600

    Completion message

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,9 +1,16 @@
 import './TodoCounter.css';
+import parse from 'html-react-parser';
 
 function TodoCounter({ total, completed }) {
+
+    const message = completed === total
+    ?  parse('<span>YOU HAVE COMPLETED ALL YOUR TASKS</span>')
+    :  parse(`You have completed <span>${completed}</span> of <span>${total}</span> TODO's`)
+
+
     return (
       <h1 className='TodoCounter'>
-        You have completed <span>{completed}</span> of <span>{total}</span> TODO's
+        {message}
       </h1>
     );
 }
